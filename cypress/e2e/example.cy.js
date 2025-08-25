5441dd132d7c688c8f7a4a7baf21cbe8501208a7
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-08-25 15:27:21 -0700

    added testing

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -6,3 +6,26 @@ describe('My First Test', () => {
     cy.contains('h1', 'You did it!')
   })
 })
+
+describe('Microblog Basic Functionality', () => {
+  it('shows the login form on the login page', () => {
+    cy.visit('/login')
+    cy.get('form').should('exist')
+    cy.contains('button', /login|sign in/i)
+  })
+
+  it('navigates to the home/feed page', () => {
+    cy.visit('/')
+    cy.contains(/feed|home/i)
+  })
+
+  it('shows a post creation form or button', () => {
+    cy.visit('/')
+    cy.get('button,form').contains(/post|create/i)
+  })
+
+  it('displays at least one post in the feed (if any exist)', () => {
+    cy.visit('/')
+    cy.get('[class*=post], .post, [data-test=post]').should('exist')
+  })
+})
